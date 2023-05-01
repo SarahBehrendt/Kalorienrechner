57a901063f5333d3525098c558c5d6c0c5d6fcdc
Smurf2409 <sarahelna1994@gmail.com>
2023-05-01 23:34:23 +0200

    kalorienrechner

---
+++ assets/Javascript/main.js
@@ -0,0 +1,35 @@
+function getDark() {
+    const wrapper = document.querySelector("body");
+    wrapper.classList.toggle("dark");
+  }
+
+  function grundumsatz(){
+    let weight = Number(document.querySelector("#weight").value);
+    let size = Number(document.querySelector("#size").value);
+    let age = Number(document.querySelector("#age").value);
+    let male = document.querySelector("#male").checked;
+    let female = document.querySelector("#female").checked;
+    let output = document.querySelector(".output");
+    let grundumsatz;
+  
+    if (male){
+      grundumsatz = 66.5 + (13.7 * weight) + (5 * size) - (6.8* age);
+    } else if (female){
+      grundumsatz = 655.1 + (9.6 * weight) + (1.8 * size) - (4.7 * age);
+    } else {
+      grundumsatz = "Geschlecht wählen!";
+    }
+  
+    output.innerHTML = grundumsatz.toFixed(0);
+    return grundumsatz.toFixed(0);
+  }
+
+  function gesamtumsatz(event){
+    event.preventDefault();
+    let inputPal = document.querySelector("#pal").value;
+    let output2 = document.querySelector(".output2");
+    let grundumsatzValue = grundumsatz();
+    let gesamtumsatz;
+    gesamtumsatz = inputPal * grundumsatzValue ;
+    output2.innerHTML=gesamtumsatz.toFixed(0);
+  }
